6c9001e040426ae07b62ba4810c5facb0032809b
stavoikono <stavoikono@gmail.com>
2020-06-02 15:23:55 +0300

    second commit

--- project/server.R
+++ project/server.R
@@ -2,6 +2,7 @@
 library(shiny)
 library(datasets)
 
+colors <- c("Red","Blue","Green","Orange","Magenta")
 # Server side code to handle user's selection
 shinyServer(function(input, output) {
     
@@ -15,6 +16,12 @@ shinyServer(function(input, output) {
                 main=input$population_group,
                 ylab="Death Rate per 1000 population",
                 xlab="Age Group",
-                col=I("Red"))
+                col=colors)
+        
     })
+    
+    # Propotion of death
+    output$prop <- renderPrint({round(VADeaths[,input$population_group]/sum(VADeaths[,input$population_group]),2)*100
+    })
+    
 })
